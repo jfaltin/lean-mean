53deb2eb333856f69ec355a51219ee94f024e860
vagrant <vagrant@scotchbox>
2018-07-23 03:50:45 +0000

    Test commit after enabling credential.helper store.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req,res) {
-   res.send('Hello World!');
+   res.send('Hello World! How be you?? ANSWER ME!!!');
   });
 
 var server = app.listen(3000, function() {
